a3b1696eda9193a5d80c76468711683f6a98c2f7
Arthur Ribeiro <arthur.carvalho@outlook.com.br>
2022-06-18 18:22:38 -0300

    feat: texto de apresentacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
-import Basicos from './components/Basicos';
-import Opalas from './components/Opalas';
-import PedrasCorrentes from './components/PedrasCorrentes';
-import PedrasNaturais from './components/PedrasNaturais';
-import SemPedras from './components/SemPedras';
-import Zirconias from './components/Zirconias';
+import "./App.css";
+import Basicos from "./components/Basicos";
+import Opalas from "./components/Opalas";
+import PedrasCorrentes from "./components/PedrasCorrentes";
+import PedrasNaturais from "./components/PedrasNaturais";
+import SemPedras from "./components/SemPedras";
+import Zirconias from "./components/Zirconias";
 
 function App() {
   return (
@@ -12,6 +12,18 @@ function App() {
       <div className="Logo">
         <img src="img/logo.png" alt="Nihard Piercer" />
         <p>Nihardt | Catálogo</p>
+      </div>
+
+      <div className="text">
+        <p>
+          Oie! Todas as jóias desse catalogo são em titânio grau implante
+          fabricados pela Millennium Jewelry. Por ser um material inerte, isto
+          é, compatível com nosso organismo, resistente, leve, livre de níquel
+          ou outros metais pesados, é o ideal para ficar em contato constante
+          com o corpo. É uma joia pra vida toda. Todas tem rosqueamento interno
+          e polimento espelho. São enviadas higienizadas e estéreis, junto com a
+          garantia e laudo de composição. Qualquer dúvida me fala ❤️
+        </p>
       </div>
       <Basicos />
       <Zirconias />
